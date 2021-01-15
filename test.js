4036019a2086ad0b13eb749af984889111338725
Aga Skorupa <aga.skorupa@yahoo.pl>
2021-01-15 10:59:34 +0100

    Else If Statements

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-var youLikeMeat = true;
-var myNum = 10;
+var myAge = 35;
 
-if (youLikeMeat){
-
-    document.write("you like meat");
-
-} else{
-
-    document.write("you hate meat")
-}
+if (myAge > 30){
+    document.write("you are over 30");
+    } else if (myAge > 20){
+        document.write("you are over 20");
+    } else if (myAge > 10){
+        document.write("you are over 10");
+    } else{
+        document.write("you are not over 10");
+    }
