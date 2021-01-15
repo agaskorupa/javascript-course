e51ecb21ffab0173e2c69b26be08a87dc4d93618
Aga Skorupa <aga.skorupa@yahoo.pl>
2021-01-15 11:58:46 +0100

    Logical Operators

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
-var myAge = 35;
+var myAge = 25;
 
-if (myAge > 30){
-    document.write("you are over 30");
-    } else if (myAge > 20){
-        document.write("you are over 20");
-    } else if (myAge > 10){
-        document.write("you are over 10");
-    } else{
-        document.write("you are not over 10");
-    }
+if (myAge < 18 || myAge > 30){
+
+    document.write("you aint coming");
+    
+
+} else{
+
+    document.write("You can come, you cool dude");
+    
+}
+
