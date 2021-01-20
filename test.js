3b2746d9fed9aa7efc307eac82ea721ccbac510b
Aga Skorupa <aga.skorupa@yahoo.pl>
2021-01-20 09:05:09 +0100

    Creating a new Javascript Object Lesson 28

--- test.js
+++ test.js
@@ -1,4 +1,26 @@
-var string1 = "abc";
-var string2 = "bcd";
+var myArray = new Array();
+myArray[0]=8;
+myArray[1]="hello";
 
-console.log(string1 === string2);
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Shaun";
+
+console.log(myCar.driver);
+myCar.drive = function(){console.log("now driving");};
+
+myCar.drive();
+
+var myCar2 = {
+
+    maxSpeed: 70, 
+    driver: "Net ninja", 
+    drive: function(speed, time){
+        console.log(speed * time);
+    }
+
+};
+
+console.log(myCar2.maxSpeed);
+myCar2.drive(50, 3);
+
